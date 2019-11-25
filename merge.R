adbee621b0d9f75844f9bee1cee018fdc17f9a9b
Jocelyn Pender <pender.jocelyn@gmail.com>
2019-11-25 16:40:55 -0500

    Create merge.R

---
+++ merge.R
@@ -0,0 +1,14 @@
+# https://www.r-bloggers.com/merging-multiple-data-files-into-one-data-frame/
+multmerge = function(mypath) {
+  filenames = list.files(path = mypath, full.names = TRUE)
+  datalist = lapply(filenames, function(x) {
+    read.csv(file = x, header = T)
+  })
+  Reduce(function(x, y) {
+    merge(x, y)
+  }, datalist)
+}
+
+# Open an R console and run:
+trait_matrix <- multmerge("csv_result_files") # Path where CSV files are stored
+write.csv(trait_matrix, file = "cyperaceae_trait_matrix.csv", na = "")
