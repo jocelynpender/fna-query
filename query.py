6ded26ac499fbbc36d3dc0d2a2ec0e1fdac35fd8
Jocelyn Pender <jpend049@uottawa.ca>
2020-01-21 16:21:24 -0500

    add python function for running ask query and returning page titles

---
+++ query.py
@@ -0,0 +1,17 @@
+from mwclient import Site
+import pandas as pd
+import local  # create a file called local.py with your credentials
+
+
+def query_titles(query):
+    """
+
+    """
+    #  query = "[[Authority::Linnaeus]][[Distribution::Nunavut]]"
+
+    site = Site(('http', 'beta.floranorthamerica.org'))
+    site.login(local.USERNAME, local.PASSWORD)  # create a file called local.py with your credentials
+    page_titles = [answer["fulltext"] for answer in site.ask(query)]  # fulltext key is the page title
+    page_titles_data_frame = pd.DataFrame(page_titles)
+    page_titles_data_frame.to_csv("page_titles.csv")
+    return page_titles_data_frame
