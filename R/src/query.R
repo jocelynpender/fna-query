bb41810486289cd39fa36f1e04901593d33e39bf
Jocelyn Pender <jpend049@uottawa.ca>
2020-01-30 15:53:16 -0500

    add improved inline documentation to R scripts

--- R/src/query.R
+++ R/src/query.R
@@ -3,8 +3,8 @@ require("tidyverse")
 
 
 ask_query_url <- function(query_string) {
-  # Concatenate a query_string with the http api URL for a Semantic MediaWiki
-  # instance
+  # Concatenate a query_string with the http API ask query module URL
+  # for the FNA Semantic MediaWiki instance
   base_url = "http://beta.semanticfna.org/w/api.php?action=ask&query="
   url = paste(base_url, query_string, sep = "")
   return(url)
@@ -14,7 +14,7 @@ run_query <- function(query_string) {
   # Run a query against the Semantic MediaWiki http api URL and obtain results back
   # in R
   url <- ask_query_url(query_string)
-  query_results <- query(url, out_class = "none")
+  query_results <- query(url, out_class = "none") # Uses the WikipediR query function
   return(query_results)
 }
 
@@ -28,19 +28,31 @@ query_page_titles <- function(query_results) {
 
 
 query_property_texts <- function(printouts, property) {
-  # Input: query results returned by the WikipediR query function 
-  # Output: a named vector with property text as vlues and Taxon name as vector names
+  # This function is used to isolate property text from other query results 
+  # returned by the WikipediR query function and return property texts in a clean named vector
+  # that can be placed inside a dataframe.
+  # This function handles the differences in returned data between property types (string property, 
+  # page property, properties with multiple possible values)
+  # Input: 
+  # printouts: query results returned by the WikipediR query function 
+  # property: property name (e.g., Illustrator, Distribution, etc.)
+  # Output: a named vector with property text as values and Taxon name as vector names
   unlisted_printouts <- printouts %>% unlist
-  property_text_labels <- names(printouts) %>% paste(., property, sep = ".")  # paste the Taxon name returned with the property name to get the label for selecting the property text returned by the query
-  property_text <- unlisted_printouts[property_text_labels]  # select the property texts using 'Taxon name.Property name'
+  property_text_labels <- names(printouts) %>% paste(., property, sep = ".")  # paste the Taxon name
+  # with the property name to get the label for selecting the property text returned by the query
+  property_text <- unlisted_printouts[property_text_labels]  # select the property texts using
+  # 'Taxon name.Property name'. This will collect data perfectly well for string properties.
   grepped_property_text_labels = property_text_labels %>% map(~names(unlisted_printouts)[grepl(., 
-    names(unlisted_printouts))]) %>% unlist
+    names(unlisted_printouts))]) %>% unlist # This takes the above 'Taxon name.Property name'
+  # labels and matches them everywhere within the label list. If a property is stored as
+  # fulltext because it is a page type there will be more than one hit per 'Taxon name.Property name'
+  # (one each for fulltext, fullurl, namespace, etc.) or it has more than one returned value, 
+  # there will be multiple hits per label, e.g., 'Taxon name.Property name2', 'Taxon name.Property name3', ...
   if (length(grepped_property_text_labels) > length(property_text)) {
-    # If the above returns no hits, the property text may be stored as 'fulltext'
     property_text_labels_full_text <- paste(property_text_labels, "fulltext", 
-      sep = ".")
+      sep = ".") # this works for property type = page
     property_text <- unlisted_printouts[property_text_labels_full_text]
-    if (any(grepl("\\d$", grepped_property_text_labels))) {
+    if (any(grepl("\\d$", grepped_property_text_labels))) { # alternatively, this grabs text 
       # if properties are numbered
       property_text <- unlisted_printouts[grepped_property_text_labels]
     }
@@ -52,7 +64,9 @@ query_property_texts <- function(printouts, property) {
 
 
 properties_texts_to_data_frame <- function(properties_texts, properties) {
-  # Input: a list of property results with associated Taxon names 
+  # Input: 
+  # properties_texts: a list of property results with associated Taxon names as names(list)
+  # properties: the names of the properties, used to set column names
   # Output: a left-joined data frame housing all property text/Taxon name combinations
   properties_texts_list <- properties_texts %>% map(~data.frame(., names(.)))  # Build data frames
   properties_texts_list <- lapply(seq_along(properties_texts_list), function(i) setNames(properties_texts_list[[i]], 
@@ -73,13 +87,10 @@ ask_query_titles <- function(query_string, output_file_name) {
 
 
 ask_query_titles_properties <- function(query_string, output_file_name) {
-  # Input: a query string to run ask SMW ask query API 
-  # Examples: query_string =
-  # '[[Authority::Miller]]|?Taxon family|?Volume' query_string =
-  # '[[Distribution::Ont.]][[Author::Geoffrey A. Levin]]|?Taxon
-  # family|?Volume|?Illustration|?Distribution' query_string =
-  # '[[Distribution::Ont.]][[Author::Geoffrey A. Levin]]|?Taxon
-  # family|?Volume|?Distribution' 
+  # Input:
+  # output_file_name: self-explanatory
+  # query_string: a query string to run ask SMW ask query API, e.g.:
+  # '[[Distribution::Ont.]][[Author::Geoffrey A. Levin]]|?Taxon family|?Volume|?Distribution' 
   # Output: a csv and a data frame holding Taxon
   # names returned and the property values asked for
   query_results <- run_query(query_string)
