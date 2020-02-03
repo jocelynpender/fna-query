bc8c35333664ebe2ac3c5c58623dac7a068bdc39
Jocelyn Pender <jpend049@uottawa.ca>
2020-02-03 16:43:42 -0500

    refactor python functions to run faster

--- python/src/query.py
+++ python/src/query.py
@@ -34,7 +34,7 @@ def extract_property_text(printouts, property):
     return property_text
 
 
-def extract_taxon_properties(item, properties):
+def extract_taxon_properties(site, query_string, properties):
     """
 
     Parameters
@@ -44,15 +44,22 @@ def extract_taxon_properties(item, properties):
 
     Returns
     -------
+    property_texts:
+
+    # use a for loop to collect properties and other things we want to store! No need to convert the full generator
 
     """
-    taxon_name = item['fulltext']
-    printouts = item['printouts']  # the API query returns "printouts" of the requested property texts as the first
-    # element of the data
+    properties_texts = []
+    taxon_names = []
 
-    property_texts = [extract_property_text(printouts, property) for property in properties]
-    property_texts.insert(0, taxon_name)
-    return property_texts
+    for answer in site.ask(query_string):
+        for title, data in answer.items():
+            if title == 'printouts':
+                properties_texts.append([extract_property_text(data, property) for property in properties])
+            if title == 'fulltext':
+                taxon_names.append(data)
+
+    return taxon_names, properties_texts
 
 
 def ask_query(query_string, output_file_name):
@@ -60,25 +67,29 @@ def ask_query(query_string, output_file_name):
 
     Parameters
     ----------
-    query_string
+    output_file_name: output file name
+    query_string: a query string to run ask SMW ask query API, e.g.:
 
     # query_string = "[[Authority::Linnaeus]][[Distribution::Nunavut]]|?Taxon family"
     # query_string = "[[Authority::Miller]]|?Taxon family|?Volume"
     # query_string = "[[Distribution::Ont.]][[Author::Geoffrey A. Levin]]|?Taxon family|?Volume|?Illustration|?Distribution"
     # query_string = "[[Distribution::Ont.]][[Author::Geoffrey A. Levin]]|?Taxon family|?Volume|?Distribution"
 
-    output_file_name
-
     Returns
     -------
+    properties_texts_data_frame:
 
     """
     site = site_login()
-    generator = site.ask(query_string)
-    data = list(generator)
     properties = re.split(r'\|\?', query_string)[1:]
-    properties_texts = [extract_taxon_properties(item, properties) for item in data]
-    properties.insert(0, 'Taxon name')  # Convert the list to appropriate column names
-    properties_texts_data_frame = pd.DataFrame(properties_texts, columns=properties)
+    taxon_names, properties_texts = extract_taxon_properties(site, query_string, properties)
+    properties_texts = pd.DataFrame(properties_texts, columns=properties)
+    properties_texts_data_frame = pd.concat([pd.Series(taxon_names), properties_texts], axis=1)
+    properties_texts_data_frame = properties_texts_data_frame.rename(columns={0: "Taxon name"})
     properties_texts_data_frame.to_csv(output_file_name, index=False)
     return properties_texts_data_frame
+
+# TO DO
+# time the list() vs. DataFrame() vs. for loop and appending for a shorter query
+
+# query_string = "[[Illustrator::+]][[Illustration::Present]]"
