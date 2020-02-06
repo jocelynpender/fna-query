0eaf338a9072c3c8ea6ed76a1f12d8bb672bf252
Jocelyn Pender <jpend049@uottawa.ca>
2020-02-06 13:37:48 -0500

    implement while loop in R script to continue query past limit via offset

--- R/src/query.R
+++ R/src/query.R
@@ -1,31 +1,39 @@
 require("WikipediR")
 require("tidyverse")
 
-
-ask_query_url <- function(query_string) {
+ask_query_url <- function(query_string, query_param) {
   # Concatenate a query_string with the http API ask query module URL
   # for the FNA Semantic MediaWiki instance
   base_url = "http://beta.semanticfna.org/w/api.php?action=ask&query="
-  url = paste(base_url, query_string, sep = "")
+  seps <- list(pipe = "%7C", eq = "%3D")
+  prep_params <- map2(names(query_param), query_param, ~ paste(.x, .y, sep=seps$eq))
+  paste_params <- paste(prep_params, collapse=seps$pipe)
+  url <- paste(base_url, query_string, paste_params, sep = "")
   return(url)
 }
 
 run_query <- function(query_string) {
-  # Run a query against the Semantic MediaWiki http api URL and obtain results back
-  # in R
-  url <- ask_query_url(query_string)
-  query_results <- query(url, out_class = "none") # Uses the WikipediR query function
-  return(query_results)
+  # Run a query against the Semantic MediaWiki http api URL and obtain results back in R
+  query_offset = 0
+  query_results_list <- list() # Need to initialize the list, then add to it in the loop
+  while (!is.null(query_offset) == TRUE) { # While there continues to be an offset...
+    url <- ask_query_url(query_string, query_param = list(limit = "500", offset = query_offset))
+    query_results <- query(url, out_class = "none") # Uses the WikipediR query function
+    query_results_list[[length(query_results_list) + 1]] <- query_results$query$results
+    query_offset <- query_results$`query-continue-offset`
+  }
+  query_results_list <- do.call(c, query_results_list) # Fix up list formatting
+  return(query_results_list)
 }
 
 query_page_titles <- function(query_results) {
+  # Return page titles that match a query
   # Input: query results returned by the WikipediR query function 
   # Output: a list of page title results
-  page_titles <- query_results$query$results %>% map(~.$fulltext) %>% unlist
+  page_titles <- query_results %>% map(~.$fulltext) %>% unlist
   page_titles_vector <- as.vector(page_titles)
   return(page_titles_vector)
 }
-
 
 query_property_texts <- function(printouts, property) {
   # This function is used to isolate property text from other query results 
@@ -62,8 +70,8 @@ query_property_texts <- function(printouts, property) {
   return(property_text)
 }
 
-
 properties_texts_to_data_frame <- function(properties_texts, properties) {
+  # Transform property texts into a dataframe
   # Input: 
   # properties_texts: a list of property results with associated Taxon names as names(list)
   # properties: the names of the properties, used to set column names
@@ -85,20 +93,29 @@ ask_query_titles <- function(query_string, output_file_name) {
   return(page_titles_vector)
 }
 
+query_string_to_data_frame <- function(query_string, query_results) {
+  # Run the query string to property texts dataframe pipeline
+  # Input: query string
+  # Output: a dataframe containing property text data with names
+  properties <- strsplit(query_string, "\\|\\?") %>% map(~.[2:length(.)]) %>% unlist
+  printouts <- query_results %>% map(~.$printouts)  # the API query returns 'printouts' of the requested property texts
+  properties_texts <- properties %>% map(~query_property_texts(printouts, property = .))
+  properties_texts_data_frame <- properties_texts_to_data_frame(properties_texts, 
+                                                                properties)
+  return(properties_texts_data_frame)
+}
 
 ask_query_titles_properties <- function(query_string, output_file_name) {
+  # Run the entire workflow from query string to output file
   # Input:
-  # output_file_name: self-explanatory
+  # output_file_name: output file name
   # query_string: a query string to run ask SMW ask query API, e.g.:
   # '[[Distribution::Ont.]][[Author::Geoffrey A. Levin]]|?Taxon family|?Volume|?Distribution' 
   # Output: a csv and a data frame holding Taxon
   # names returned and the property values asked for
   query_results <- run_query(query_string)
-  properties <- strsplit(query_string, "\\|\\?") %>% map(~.[2:length(.)]) %>% unlist
-  printouts <- query_results$query$results %>% map(~.$printouts)  # the API query returns 'printouts' of the requested property texts
-  properties_texts <- properties %>% map(~query_property_texts(printouts, property = .))
-  properties_texts_data_frame <- properties_texts_to_data_frame(properties_texts, 
-    properties)
+  properties_texts_data_frame <- data.frame()
+  properties_texts_data_frame <- query_string_to_data_frame(query_string, query_results)
   write.csv(properties_texts_data_frame, output_file_name)
   return(properties_texts_data_frame)
 }
